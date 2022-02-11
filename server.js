d6c39c00f26526dd39f414564fe08cc65a343320
samara-kukreja22 <55211612+samara-kukreja22@users.noreply.github.com>
2022-02-11 14:20:17 -0500

    changes

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ app.use(express.static('public')); //specify location of static assests
 app.set('views', __dirname + '/views'); //specify location of templates
 app.set('view engine', 'ejs'); //specify templating library
 
+app.get('/', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("index");
+});
+
+app.use("", function(request, response){
+  response.status(404);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("error", {
+    "errorCode":"404"
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Server started at http://localhost:'+port+'.')
